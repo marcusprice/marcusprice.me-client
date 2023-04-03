225a3c4bb502a153681ec463904a243b08ba16b4
Marcus Price <marcus.price@oracle.com>
2023-04-02 18:03:30 -0700

    fixes asset paths

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
+rm -rf build
+
 # create build dir 
-mkdir -p build/assets
+mkdir -p build/assets/icons build/css build/js
+
 # compress svg icons
-svgo -f ./src/assets -o ./build/assets
+echo "compressing icons:"
+svgo -f ./src/assets/icons -o ./build/assets/icons
+
+sleep 1
+# uglify css & js
+uglifycss "./src/css/styles.css" > ./build/css/styles.css
+echo ""
+
+echo "✅  styles minified"
+echo ""
+
+sleep 1
+cp -R ./src/fonts ./build/
+echo "✅  font copied"
+echo ""
+
+sleep 1
+uglifyjs "./src/js/main.js" > ./build/js/main.js
+echo "✅  js minified"
+echo ""
+
+sleep 1
 # copy index to build 
 cp ./src/index.html ./build
+echo "✅  html document copied"
+echo ""
+
+sleep 1
+echo "🥳  all done"
